6c4bcb745d8026782147d1a949e0cb284682d99a
James Quick <james.q.quick@fedex.com>
2019-02-25 08:48:09 -0600

    Added new components to display return data and endpoing and data form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Nav } from './components/Nav';
+import styled from 'styled-components';
+import { DataForm } from './components/DataForm';
+import {EndpointForm} from './components/EndpointForm';
+import {DataDisplay} from './components/DataDisplay';
+class App extends Component<{children}, {data}> {
 
-class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {data:{}}
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledApp className="App">
+        <Nav />
+        {this.props.children}
+      </StyledApp>
     );
   }
+
+  handleDataReceived = (data:any) => {
+    console.log(data)
+    this.setState({data});
+  }
 }
+
+const StyledApp = styled.div`
+  margin-left: 200px;
+  padding: 50px;
+`
 
 export default App;
