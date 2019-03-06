0a06dd3a46ba2e9aee3e7a7ebf308c304b88262a
James Quick <james.q.quick@outlook.com>
2019-03-06 13:21:05 -0600

    Added components for display users apis

--- functions/api.js
+++ functions/api.js
@@ -31,6 +31,10 @@ exports.handler = function(event, context, callback) {
           });
         }
       }
+      return callback(null, {
+        statusCode: 404,
+        body: JSON.stringify({ msg: "Data not found" })
+      });
     })
     .catch(err => {
       console.log(err);
@@ -39,32 +43,4 @@ exports.handler = function(event, context, callback) {
         body: JSON.stringify({ msg: "Something went wrong!" })
       });
     });
-
-  console.log("hehe");
-  //   const userId = req.params.userId;
-
-  //   console.log(urlParts);
-  //   if (urlParts.length != 4) {
-  //     return res.status(400).send({ msg: `Bad request` });
-  //   }
-  //   const url = "/" + urlParts[2] + "/" + urlParts[3];
-  //   db.ref(`users/${userId}/apis`)
-  //     .orderByChild("url")
-  //     .equalTo(url)
-  //     .once("value")
-  //     .then(snapshot => {
-  //       console.log(snapshot.val());
-  //       if (!snapshot.val()) {
-  //         return res.status(404).send({ msg: `Could not find data for ${url}` });
-  //       }
-  //       const data = snapshot.val();
-  //       const keys = Object.keys(data);
-  //       const retVal = data[keys[0]];
-  //       console.log(retVal);
-  //       res.status(retVal.status).send(retVal.response);
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //       res.status(500).send({ msg: `Could not retrieve data for ${url}` });
-  //     });
 };
