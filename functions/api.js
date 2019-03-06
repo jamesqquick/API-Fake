0ae5fb6be81beb30b12350d5e8ac4db42dca6666
James Quick <james.q.quick@outlook.com>
2019-03-06 11:54:46 -0600

    Added function for handling api requests

--- functions/api.js
+++ functions/api.js
@@ -1,59 +1,66 @@
-//require('dotenv').config()
+const axios = require("axios");
 
-const firebase = require('firebase');
+exports.handler = function(event, context, callback) {
+  console.log("****************");
+  const urlParts = event.path.split("/");
+  const userId = urlParts[2];
+  console.log(userId);
+  const url = "/" + urlParts.slice(3).join("/");
+  console.log(url);
+  const request_url = `https://api-fake-test.firebaseio.com/users/${userId}/apis.json`;
 
-const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  axios
+    .get(request_url)
+    .then(res => {
+      console.log(res.data);
+      for (let key in res.data) {
+        console.log(res.data[key].url, url);
+        if (res.data[key].url == url) {
+          return callback(null, {
+            statusCode: 200,
+            body: res.data[key].response
+          });
+        } else {
+          return callback(null, {
+            statusCode: 404,
+            body: JSON.stringify({ msg: "Data not found" })
+          });
+        }
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ msg: "Something went wrong!" })
+      });
+    });
+
+  console.log("hehe");
+  //   const userId = req.params.userId;
+
+  //   console.log(urlParts);
+  //   if (urlParts.length != 4) {
+  //     return res.status(400).send({ msg: `Bad request` });
+  //   }
+  //   const url = "/" + urlParts[2] + "/" + urlParts[3];
+  //   db.ref(`users/${userId}/apis`)
+  //     .orderByChild("url")
+  //     .equalTo(url)
+  //     .once("value")
+  //     .then(snapshot => {
+  //       console.log(snapshot.val());
+  //       if (!snapshot.val()) {
+  //         return res.status(404).send({ msg: `Could not find data for ${url}` });
+  //       }
+  //       const data = snapshot.val();
+  //       const keys = Object.keys(data);
+  //       const retVal = data[keys[0]];
+  //       console.log(retVal);
+  //       res.status(retVal.status).send(retVal.response);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //       res.status(500).send({ msg: `Could not retrieve data for ${url}` });
+  //     });
 };
-const firebaseApp = firebase.initializeApp(config);
-const db = firebaseApp.database();
-
-app.post('/data', (req, res) => {
-    console.log(req.body);
-    const { url = null, response = null, status = null } = req.body;
-    console.log(status);
-    //Check for valid input
-    if (!url || !response || typeof (response) != "object" || !status) {
-        console.log("Bad Request")
-        return res.status(400).send(
-            "Bad Request"
-        )
-    }
-    db[url] = { response, status };
-    res.send(db[url]);
-});
-
-
-app.all('/:userId/*', (req, res) => {
-
-    const userId = req.params.userId;
-
-    const urlParts = req.url.split("/");
-    console.log(urlParts);
-    if (urlParts.length != 4) {
-        return res.status(400).send({ msg: `Bad request` });
-    }
-    const url = "/" + urlParts[2] + "/" + urlParts[3];
-
-    db.ref(`users/${userId}/apis`).orderByChild('url').equalTo(url).once('value')
-        .then(snapshot => {
-            console.log(snapshot.val());
-            if (!snapshot.val()) {
-                return res.status(404).send({ msg: `Could not find data for ${url}` });
-            }
-            const data = snapshot.val();
-            const keys = Object.keys(data);
-            const retVal = data[keys[0]];
-            console.log(retVal);
-            res.status(retVal.status).send(retVal.response);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send({ msg: `Could not retrieve data for ${url}` });
-        })
-})
