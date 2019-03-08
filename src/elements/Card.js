f9664efa20cdee71fdb28e63f8c1f2b491cfa4f9
James Quick <james.q.quick@fedex.com>
2019-03-08 13:58:14 -0600

    Updated test data page to hopefully query right url. Updated api function to better parse input url

---
+++ src/elements/Card.js
@@ -0,0 +1,11 @@
+
+import styled from "styled-components";
+import { darkGray } from "../utilities";
+
+export const Card = styled.div`
+    padding: 40px;
+    background-color: white;
+    border: 1px solid ${darkGray};
+    border-radius:5px;
+    margin: 20px 0;
+`
---
+++ src/elements/Code.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Button } from "../elements/Button";
+import { Form } from "../elements/Form";
+import styled from "styled-components";
+
+
+export const Code = styled.pre`
+  background: white;
+  border-radius: 4px;
+  padding: 40px 10px;
+  width:100%;
+  max-width:600px;
+  margin: 40px auto;
+`
+
+
