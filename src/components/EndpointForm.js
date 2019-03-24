bb4439a595e26980aaa4b263bfe1bd6b98aa4094
James Quick <james.q.quick@outlook.com>
2019-03-24 18:52:21 -0500

    Placeholder text for endpoint form

--- src/components/EndpointForm.js
+++ src/components/EndpointForm.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Button } from "../elements/Button";
 import { Form } from "../elements/Form";
 import { Card } from "../elements/Card";
 class EndpointForm extends Component {
-
   constructor(props) {
     super(props);
     this.state = { url: "", response: null };
-
   }
   render() {
     return (
       <>
-        <Form >
+        <Form>
           <label htmlFor="url">URL</label>
-          <input type="text" name="url" onChange={this.onChange} />
-          <Button type="submit" onClick={this.onSubmit}>Test!</Button>
+          <input
+            type="text"
+            name="url"
+            onChange={this.onChange}
+            placeholder="ex. /posts/1"
+          />
+          <Button type="submit" onClick={this.onSubmit}>
+            Test!
+          </Button>
         </Form>
-        {
-          this.state.response && (
-            <Card>{JSON.stringify(this.state.response)}</Card>
-          )
-        }
+        {this.state.response && (
+          <Card>{JSON.stringify(this.state.response)}</Card>
+        )}
       </>
-    )
+    );
   }
 
-  onChange = (event) => {
+  onChange = event => {
     this.setState({
-      "url": event.target.value
-    })
-  }
+      url: event.target.value
+    });
+  };
 
-  onSubmit = async (event) => {
+  onSubmit = async event => {
     event.preventDefault();
     try {
-      const url = `/.netlify/functions/api/${this.props.user.uid}${this.state.url}`
+      const url = `/.netlify/functions/api/${this.props.user.uid}${
+        this.state.url
+      }`;
       console.log(url);
       const res = await fetch(url);
       const data = await res.json();
-      this.setState({ response: data })
+      this.setState({ response: data });
       //this.props.dataRetrieved(data);
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 }
 
-
-
 export default EndpointForm;
-
-
